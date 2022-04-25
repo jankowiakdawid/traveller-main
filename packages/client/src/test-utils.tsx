84c5aa6abe091fd6c3d7d502b168cd7032d106d8
Dawid Jankowiak <jankowiak.dawid@gmail.com>
2022-04-25 22:41:30 +0200

    Refactor to make passing mocked responses possible

--- packages/client/src/test-utils.tsx
+++ packages/client/src/test-utils.tsx
@@ -4,13 +4,13 @@ import { render } from '@testing-library/react'
 import { ChakraProvider, theme } from '@chakra-ui/react'
 import { MockedProvider } from '@apollo/client/testing'
 
-const AllProviders = ({ children }: { children?: React.ReactNode }) => (
-  <MockedProvider addTypename={false}>
-    <ChakraProvider theme={theme}>{children}</ChakraProvider>
-  </MockedProvider>
-)
+const customRender = (ui: React.ReactElement, options?: RenderOptions): void => {
+  const AllProviders = ({ children }: { children?: React.ReactNode }) => (
+    <MockedProvider addTypename={false} mocks={options?.mocks ?? []}>
+      <ChakraProvider theme={theme}>{children}</ChakraProvider>
+    </MockedProvider>
+  )
 
-const customRender = (ui: React.ReactElement, options?: RenderOptions) =>
   render(ui, { wrapper: AllProviders, ...options })
-
+}
 export { customRender as render }
