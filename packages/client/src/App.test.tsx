cce3da9400d885bd0e17b235cbb37660aa2d8972
Dawid Jankowiak <jankowiak.dawid@gmail.com>
2022-04-25 20:32:58 +0200

    Fix test by wrapping App in Router
    Will probably need to put it in utils
    when I start fetch data

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { screen } from '@testing-library/react'
+import { createMemoryHistory } from 'history'
+import { BrowserRouter as Router } from 'react-router-dom'
+
 import { render } from './test-utils'
 import { App } from './App'
 
 describe('<App /> component', () => {
   it('renders the Header content', () => {
-    render(<App />)
+    const history = createMemoryHistory()
+    history.push('/')
+    render(
+      <Router history={history}>
+        <App />
+      </Router>
+    )
     const HeadingComponent = screen.getByText(/^Smart traveller$/i)
     expect(HeadingComponent).toBeInTheDocument()
   })
