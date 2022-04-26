8c6445c026696b296f677c9b190d60cfeb0967f0
Dawid Jankowiak <jankowiak.dawid@gmail.com>
2022-04-26 22:02:19 +0200

    Fix type of mocks option
    It wasn't typed at all and TS was complaining at every rebuild about it.
    Quite annoying

--- packages/client/src/test-utils.tsx
+++ packages/client/src/test-utils.tsx
@@ -3,10 +3,14 @@ import type { RenderOptions } from '@testing-library/react'
 import { render } from '@testing-library/react'
 import { ChakraProvider, theme } from '@chakra-ui/react'
 import { MockedProvider } from '@apollo/client/testing'
+import type { MockedResponse } from '@apollo/client/testing'
 
-const customRender = (ui: React.ReactElement, options?: RenderOptions): void => {
+const customRender = (
+  ui: React.ReactElement,
+  options?: RenderOptions & { mocks?: MockedResponse<Record<string, any>>[] }
+): void => {
   const AllProviders = ({ children }: { children?: React.ReactNode }) => (
-    <MockedProvider addTypename={false} mocks={options?.mocks ?? []}>
+    <MockedProvider addTypename={false} mocks={options?.mocks ?? ([] as MockedResponse<Record<string, any>>[])}>
       <ChakraProvider theme={theme}>{children}</ChakraProvider>
     </MockedProvider>
   )
