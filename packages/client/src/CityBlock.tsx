272f4a857ed1556226d13c68fb970db19ed823b8
Dawid Jankowiak <jankowiak.dawid@gmail.com>
2022-04-26 23:34:58 +0200

    Abstract away button with icon and checkbox

--- packages/client/src/CityBlock.tsx
+++ packages/client/src/CityBlock.tsx
@@ -5,6 +5,21 @@ import { useMutation } from '@apollo/client'
 import { VISIT_CITY } from './queries'
 import type { City } from './queries'
 
+type BtnProps = {
+  children: React.ReactNode
+  checked: boolean
+  onClick: () => void
+}
+
+function CheckedButton({ children, checked, onClick }: BtnProps) {
+  return (
+    <button onClick={onClick}>
+      <input type="checkbox" checked={checked} readOnly style={{ height: 0, width: 0, opacity: 0 }} />
+      {children}
+    </button>
+  )
+}
+
 type Props = {
   city: City
 }
@@ -26,10 +41,12 @@ export function CityBlock({ city }: Props): JSX.Element {
       paddingX={4}
       paddingY={2}
     >
-      <button onClick={() => visitCity({ variables: { id: city.id, visited: !city.visited } })}>
-        <input type="checkbox" checked={city.visited} readOnly style={{ height: 0, width: 0, opacity: 0 }} />
+      <CheckedButton
+        checked={city.visited}
+        onClick={() => visitCity({ variables: { id: city.id, visited: !city.visited } })}
+      >
         <Icon as={CheckCircleIcon} w={6} h={6} color={visitedColor} />
-      </button>
+      </CheckedButton>
       <Box textAlign="left">
         <Text fontSize="3xl">{city.name}</Text>
         <Text color="gray.500" fontSize="xl">
